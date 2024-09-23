30a0684ede1073f688796de4506398e2eb1bce7b
carmenchurg <carmen.tandemaran@gmail.com>
2024-09-23 13:09:52 +0200

    nuevo

---
+++ src/pages/contacto.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Link } from 'gatsby';
+
+const ContactoPage = () => (
+  <main>
+    <h1>Contacto</h1>
+    <p>Aquí puedes contactarme.</p>
+    <Link to="/">Volver al Inicio</Link>
+  </main>
+);
+
+export default ContactoPage;
